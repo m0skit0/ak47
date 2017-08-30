c1f86d366a0fb990eebedd6345971ae6507e3f7d
yasin <yassine.diouri@zkteco.eu>
2017-08-30 17:11:45 +0200

    Added the following functions: - File.installAPKNoPrompt() - File.md5() - String.runCommand() - String.runCommandAsSu()

--- src/org/m0skit0/android/ak47/FileUtils.kt
+++ src/org/m0skit0/android/ak47/FileUtils.kt
@@ -3,6 +3,7 @@ package org.m0skit0.android.ak47
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import java.io.File
 
@@ -15,14 +16,41 @@ fun File.getNameWithoutExtension() = this.name.substringBefore('.')
 /**
  * Installs file as APK. Asks for user permission.
  */
-fun File.installAPK(context: Context): File {
-    val intent = Intent(Intent.ACTION_INSTALL_PACKAGE)
-    intent.data = Uri.fromFile(this)
-    context.startActivity(intent)
-    return this
-}
+fun File.installAPK(context: Context) = apply {
+        val intent = Intent(Intent.ACTION_INSTALL_PACKAGE)
+        intent.data = Uri.fromFile(this)
+        context.startActivity(intent)
+    }
+
+/**
+ * Installs file as APK without asking permission.
+ * NOTE: This call needs superuser privileges.
+ */
+fun File.installAPKNoPrompt() = apply {
+            "pm install -r ${this.absolutePath}".runCommandAsSu()
+        }
+/**
+ * Installs file as system APK without asking permission.
+ * NOTE: Depending on the Android version, it might require a reboot.
+ * NOTE: This call needs superuser privileges.
+ */
+fun File.installSystemAPK() = apply {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP ) {
+            "cp $absolutePath /system/app".runCommandAsSu()
+        } else {
+            val apkDirectory = "/system/priv-app/${getNameWithoutExtension()}"
+            "mkdir $apkDirectory".runCommandAsSu()
+            "cp $absolutePath $apkDirectory".runCommandAsSu()
+        }
+    }
 
 /**
  * Returns standard Android download public directory.
  */
 fun getDownloadDirectory() = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+/**
+ * Calculates this file's MD5 hash.
+ * @return MD5 hash of file as string.
+ */
+fun File.md5() = "md5 $absolutePath".runCommand().substringBefore(' ')
--- src/org/m0skit0/android/ak47/StringUtils.kt
+++ src/org/m0skit0/android/ak47/StringUtils.kt
@@ -7,12 +7,41 @@ private val STRING_RESOURCE_IDENTIFIER_REGEX = "@string/(.*)".toRegex()
 
 /**
  * Returns a string resource by id or the same string if not a resource identifier.
- * @param context Android Context for Resources
+ * @param context Android Context for Resources.
+ * @return String resource that has that name or the same name if none found.
  */
 fun String.getStringResourceByName(context: Context): String {
     val id = STRING_RESOURCE_IDENTIFIER_REGEX.matchEntire(this)?.groups?.get(1)?.value ?: return this
     return context.getString(context.resources.getIdentifier(id, "string", context.packageName))!!
 }
 
+/**
+ * Returns a View by its string name (not integer identifier).
+ * @param activity Android Activity where the View is present.
+ * @return View that has that name or null if no View found
+ */
 fun String.findViewByName(activity: Activity) =
         activity.findViewById(activity.resources.getIdentifier(this, "id", activity.packageName))
+
+private fun List<String>.runCommand() =
+        Runtime.getRuntime().exec(toTypedArray()).apply { waitFor() }.inputStream.bufferedReader().readText()
+
+/**
+ * Executes command on shell and returns standard output.
+ * @return Standard output of the command.
+ */
+fun String.runCommand() =
+        Runtime.getRuntime().exec(this).apply { waitFor() }.inputStream.bufferedReader().readText()
+
+/**
+ * Executes command on shell as root (superuser) and returns standard output.
+ * NOTE: This call needs superuser privileges.
+ * @return Standard output of the command.
+ */
+fun String.runCommandAsSu() =
+        listOf("su", "-c", this).runCommand()
+
+
+
+
+
